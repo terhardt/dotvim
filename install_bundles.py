b3d1b996fb17eb1648af3c6b1572ba90ccffca12
Tobias Erhardt <tobias.erhardt@gmail.com>
2012-06-25 15:40:04 +0200

    Fixed install script

--- install_bundles.py
+++ install_bundles.py
@@ -4,26 +4,29 @@ install bundles as git submodules
 import subprocess as sp
 
 bundles = {
-        'vim-markdown'  : 'http://github.com/plasticboy/vim-markdown/',
-        'Latex-Box'     : 'http://github.com/vim-scripts/LaTeX-Box',
-        'pathogen'      : 'http://github.com/vim-scripts/pathogen.vim',
-        'The-NERD-tree' : 'http://github.com/vim-scripts/The-NERD-tree',
-        'supertab'      : 'http://github.com/ervandew/supertab',
-        'UltiSnips'     : 'http://github.com/vim-scripts/UltiSnips',
-        'fugitive'      : 'https://github.com/tpope/vim-fugitive',
+        'vim-markdown'  : 'git://github.com/plasticboy/vim-markdown.git',
+        'Latex-Box'     : 'git://github.com/vim-scripts/LaTeX-Box.git',
+        'pathogen'      : 'git://github.com/vim-scripts/pathogen.vim.git',
+        'The-NERD-tree' : 'git://github.com/vim-scripts/The-NERD-tree.git',
+        'supertab'      : 'http://github.com/ervandew/supertab/',
+        'UltiSnips'     : 'http://github.com/vim-scripts/UltiSnips/',
+        'fugitive'      : 'https://github.com/tpope/vim-fugitive/',
         'vim-powerline' : 'https://github.com/Lokaltog/vim-powerline/',
-        'tabular'       : 'https://github.com/godlygeek/tabular',
-        'tcomment_vim'  : 'http://github.com/tomtom/tcomment_vim',
-        'delimitMate'   : 'https://github.com/vim-scripts/delimitMate.vim',
-        'vim-surround'  : 'http://github.com/tpope/vim-surround',
-        'vim-solarized' : 'http://github.com/altercation/vim-colors-solarized'
+        'tabular'       : 'https://github.com/godlygeek/tabular/',
+        'tcomment_vim'  : 'http://github.com/tomtom/tcomment_vim/',
+        'delimitMate'   : 'https://github.com/vim-scripts/delimitMate.vim/',
+        'vim-surround'  : 'http://github.com/tpope/vim-surround/',
+        'vim-solarized' : 'http://github.com/altercation/vim-colors-solarized/',
+        'vim-git'       : 'https://github.com/tpope/vim-git/',
+        'gist'          : 'http://github.com/vim-scripts/Gist.vim/'
         }
 
 for bundle in bundles:
     url = bundles[bundle]
     call = 'git submodule add %s bundle/%s'%(url, bundle)
-    sp.call(call)
+    print call
+    sp.call(call, shell=True)
 
-sp.call('git submodule init')
-sp.call('git submodule foreach pull origin/master')
+sp.call('git submodule init', shell=True)
+sp.call('git submodule foreach git pull origin master', shell=True)
 
